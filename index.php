7f9f9020c1b26d52ff18e2e05b5eddd9f2f5a5a3
Rupom <mohrajulrupom@gmail.com>
2024-07-06 18:32:31 +0600

    order object

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: WC obj 
+ * Description: hello
+ * Version: 1.0
+ * Author: Rupom
+ * Text Domain: 
+ * 
+ */
+
+ function wc_obj_callback(){
+    
+      
+ }
+add_action( 'init', 'wc_obj_callback' );
