bf20145d62a42ee4858b72e2ff34174bcbbc314f
Rupom <mohrajulrupom@gmail.com>
2024-07-07 13:00:48 +0600

    product object

--- index.php
+++ index.php
@@ -173,7 +173,145 @@
     $order_received_url = $order->get_checkout_order_received_url();
     // echo $order_received_url;
     $view_order_url = $new_order->get_view_order_url();
-    echo $view_order_url;
+    // echo $view_order_url;
     // $order->get_edit_order_url();
  }
 add_action( 'init', 'wc_obj_callback' );
+
+
+//product object 
+
+function wc_product_obj_callback(){
+    $product = wc_get_product(65);
+    $product_id = $product->get_id();
+    // echo $product_id;
+  
+    $product_type = $product->get_type();
+    $product_name = $product->get_name();
+    $product_slug = $product->get_slug();
+    // print_r($product_type . $product_name . $product_slug);
+    $date_created = $product->get_date_created();
+    // print_r($date_created);
+    $date_modify = $product->get_date_modified();
+    // print_r($date_modify);
+    $product_stutas = $product->get_status();
+    // print_r($product_stutas);
+    $product_featured = $product->get_featured();
+    // echo $product_featured;
+    // echo $product->get_catalog_visibility();
+
+    $product_description = $product->get_description();
+    update_post_meta( $product_id, 'description', $product_description);
+    $get_description = get_post_meta( $product_id, 'description', true );
+    // print_r($get_description);
+    // echo $product_description;
+    $short_description = $product->get_short_description();
+    // echo $short_description;
+    $product_sku = $product->get_sku();
+    // echo $product_sku;
+    $menu_order = $product->get_menu_order();
+    // echo $menu_order;
+    // $get_virtual = $product->get_virtual();
+    // echo $get_virtual;
+    $product_permalink = get_permalink( $product_id);
+    // echo $product_permalink;
+
+    
+}
+add_action( 'init', 'wc_product_obj_callback' );
+
+// $args = array(
+//     'post_type'      => 'product',
+//     'posts_per_page' => 10,
+//     'product_cat'    => 'accessories'
+// );
+// $all_products = get_posts( $args );
+// foreach($all_products as $a_product){
+//     $id = get_the_ID();
+//     echo $id;
+    // $a_product_name = $a_product->get_name();
+    // echo $a_product_name;
+// }
+
+
+function product_obj_callback(){
+    $all_product = wc_get_products(array(
+        'category' => array( 'accessories' ),
+        // 'orderby'  => 'name',
+    ));
+    foreach($all_product as $a_product){
+        $product_info =  $a_product->get_id() . ' ' .  $a_product->get_name() . '<br>';
+        $product_details = '';
+        if($a_product->get_name() == 'Cap'){
+            $product_price = $a_product->get_price();
+            $product_details = $product_price . '<br>';
+            $regular_price = $a_product->get_regular_price();
+            $product_details .= $regular_price . '<br>';
+            $sale_price = $a_product->get_sale_price();
+            $product_details .= $sale_price; 
+
+            // echo $a_product->get_total_sales();
+            $tax_status = $a_product->get_tax_status();
+            // echo $tax_status;
+            $tax_class = $a_product->get_tax_class();
+            // echo $tax_class;
+            $stock_quantity = $a_product->get_stock_quantity();
+            // echo $stock_quantity;
+            // $product->get_stock_status();
+            // $product->get_backorders();
+            // $product->get_sold_individually();
+            // $product->get_purchase_note();
+            // $product->get_shipping_class_id();
+          
+            $product_weight = $a_product->get_weight();
+            // echo $product_weight;
+            $product_length = $a_product->get_length();
+            // echo $product_length;
+            // $a_product->get_width();
+            // $a_product->get_height();
+            $get_dimensions = $a_product->get_dimensions();
+            // echo $get_dimensions;
+
+            $get_downloads = $a_product->get_downloads();
+            // echo $get_downloads;
+            // $a_product->get_download_expiry();
+            $downloadable = $a_product->get_downloadable();
+            // echo $downloadable;
+            $download_limit = $a_product->get_download_limit();
+            // echo $download_limit;
+            
+            // Get Product Images
+            
+            $image_id = $a_product->get_image_id();
+            // echo $image_id;
+            $get_image = $a_product->get_image();
+            $gallery_image_id = $a_product->get_gallery_image_ids();
+            // print_r($gallery_image_id);
+            
+            // Get Product Reviews
+            
+            $get_reviews = $a_product->get_reviews_allowed();
+            // echo $get_reviews;
+            $rating_count = $a_product->get_rating_counts();
+            // print_r($rating_count) ;
+            // $a_product->get_average_rating();
+            // $a_product->get_review_count();
+        }
+        // echo $product_details;
+       
+    }
+
+    $order = wc_get_order( 106 );
+    $items = $order->get_items();
+    foreach ( $items as $item ) {
+        $product = $item->get_product();
+        $product->get_id();
+        $product_name = $product->get_name();
+        // echo $product_name . '<br>';
+        // echo $item->get_name();
+        echo $item->get_quantity();
+    }
+}
+add_action( 'init','product_obj_callback' );
+
+
